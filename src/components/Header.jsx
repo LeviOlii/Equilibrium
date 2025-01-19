d1d120ae10de836fb6f173e1f1bf06f4decb3fe7
LeviOlii <levi.bernardo0909@gmail.com>
2025-01-18 21:12:31 -0300

    header concluido para desktop, estilização global adicionada no index.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,51 @@
 import React, { useState } from "react";
-import "../index.css";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="bg-blue-500 text-white p-4 font-dmSans bg-brand-green">
-      <div className="container mx-aut flex justify-between items-center">
+    <header className="bg-brand-green text-white p-6 font-dmSans tracking-wide">
+      <div className=" hidden md:flex container justify-center items-center min-w-full">
         {/* Logo */}
-        <h1 className="text-2xl font-bold">Minha Plataforma</h1>
+        <h1 className="absolute top-2/ left-36 text-2xl tracking-wide">
+          Equilibrium
+        </h1>
 
         {/* Menu para Desktop */}
-        <nav className="hidden md:flex space-x-6">
+        <nav className="hidden md:flex space-x-6 justify-center items-center ">
           <a href="#home" className="hover:underline">
             Início
           </a>
           <a href="#about" className="hover:underline">
             Sobre
           </a>
-          <a href="#contact" className="hover:underline">
-            Contato
+          <a href="#groups" className="hover:underline">
+            Grupos
+          </a>
+          <a href="#testimony" className="hover:underline">
+            Depoimentos
           </a>
         </nav>
 
-        {/* Botão de Menu para Mobile */}
-        <button
-          className="md:hidden"
-          onClick={() => setMenuOpen(!menuOpen)}
-          aria-label="Abrir menu"
-        >
-          <span className="material-icons">menu</span>
+        <button className="hidden md:block absolute top-3 right-16 border-2 text-white px-8 py-2 rounded-3xl mr-4">
+          BUSQUE AJUDA
         </button>
       </div>
 
       {/* Menu Mobile */}
       {menuOpen && (
-        <nav className="md:hidden bg-blue-600 p-4 mt-2 space-y-2">
+        <nav className="md:hidden  p-4 mt-2 space-y-2">
           <a href="#home" className="block hover:underline">
             Início
           </a>
           <a href="#about" className="block hover:underline">
             Sobre
           </a>
-          <a href="#contact" className="block hover:underline">
-            Contato
+          <a href="#groups" className="block hover:underline">
+            Grupos
+          </a>
+          <a href="#testimony" className="block hover:underline">
+            Depoimentos
           </a>
         </nav>
       )}
