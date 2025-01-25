5ee16c3708af16749458a9584b65d85db353a129
LeviOlii <levi.bernardo0909@gmail.com>
2025-01-25 13:45:40 -0300

    add: biblioteca fontawesome (icons) e icone de menu da tela mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -13,7 +15,7 @@ const Header = () => {
 
         {/* Menu para Desktop */}
         <nav className="hidden md:flex md:space-x-6">
-        <Link to="/" className="hover:underline">Início</Link>
+          <Link to="/" className="hover:underline">Início</Link>
           <Link to="/about" className="hover:underline">Sobre</Link>
           <Link to="/groups" className="hover:underline">Grupos</Link>
           <Link to="/testimony" className="hover:underline">Depoimentos</Link>
@@ -30,24 +32,20 @@ const Header = () => {
           onClick={() => setMenuOpen(!menuOpen)}
           aria-label="Abrir menu"
         >
-          <span className="material-icons">menu</span>
+          <FontAwesomeIcon icon={faBars} className="text-2xl"/>
         </button>
       </div>
 
       {/* Menu Mobile */}
       {menuOpen && (
-        <nav className="md:hidden grid bg-brand-green mt-2 space-y-4 p-6 col-span-5 text-2xl">
-          <Link to="/" className="block hover:underline text-center font-bold py-4">
-            Início
+        <nav className="md:hidden grid bg-brand-green mt-2 space-y-4 p-6 col-FontAwesomeIcon-5 text-2xl">
+          <Link to="/" className="block hover:underline text-center font-bold py-4">Início
           </Link>
-          <Link to="/about" className="block hover:underline text-center font-bold py-4">
-            Sobre
+          <Link to="/about" className="block hover:underline text-center font-bold py-4">Sobre
           </Link>
-          <Link to="/groups" className="block hover:underline text-center font-bold py-4">
-            Grupos
+          <Link to="/groups" className="block hover:underline text-center font-bold py-4">Grupos
           </Link>
-          <Link to="/testimony" className="block hover:underline text-center font-bold py-4">
-            Depoimentos
+          <Link to="/testimony" className="block hover:underline text-center font-bold py-4">Depoimentos
           </Link>
           <button className="border-2 border-white py-3 rounded-full hover:bg-brand-green-hover hover:text-brand-green font-bold transition">
           BUSQUE AJUDA
