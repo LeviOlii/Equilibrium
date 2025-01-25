0ac52173f27805f3595c058a1fa70ba6d2a6f65e
Eduarda Alves <alves.maria.es@gmail.com>
2025-01-25 14:34:38 -0300

    página de login versão mobile

--- src/components/AccountLogin.jsx
+++ src/components/AccountLogin.jsx
@@ -4,18 +4,24 @@ const AccountLogin = () => {
 
   return (
     <>
-      <body className="bg-desktop-bg">
-        <div className="loginContainer text-center border-solid border-1 bg-brand-beige text-black rounded-2xl font-dmSans italic mx-7 my-12 shadow-md">
-          <h1 className="text-gray-headline text-2xl p-6">Seja bem-vindo(a)!</h1>
+      <body className="bg-desktop-bg h-screen flex items-center justify-center">
+        <div className="loginContainer text-center border-solid border-1 bg-brand-white text-black rounded-2xl font-dmSans font-extralight shadow-2xl">
 
-            <form action="action_page.php" method="post">
-                <input type="text" placeholder="Insira seu email" name="email" id="email" className="bg-mobile-bg my-2 p-1 border-2 border-solid rounded shadow-md px-14 text-center" required/>
+            <form className="mx-10 my-36" action="action_page.php" method="post">
+                <h1 className="text-gray-headline font-thin text-3xl my-6 italic">Seja bem-vindo(a)!</h1>
+
+                <div className="user_icon w-12 h-12 mx-auto my-5">
+                  <img src="src\assets\icons\user.png" alt="user_icon" />
+                </div>
+
+                <input type="text" placeholder="Insira seu email" name="email" id="email" className="bg-mobile-bg  italic p-1 border-2 border-solid rounded-xl shadow-md px-14 text-center" required/>
                 <br/>
-                <input type="password" placeholder="Insira sua senha" name="psw" className="bg-mobile-bg my-2 p-1 border-2 border-solid rounded shadow-md px-14 text-center" required/>
+                <input type="password" placeholder="Insira sua senha" name="psw" className="bg-mobile-bg italic my-6 p-1 border-2 border-solid rounded-xl shadow-md px-14 text-center" required/>
                 <br/>
                 <a className="text-gray-headline" href="_blank">Esqueceu sua senha?</a>
                 <br />
-                <button className="bg-mobile-bg border-2 border-solid rounded shadow-md px-7 m-2 text-center p-1" type="submit">Login</button>
+                <button className="bg-mobile-bg border-2 border-solid rounded-xl shadow-md px-8 my-6 text-center py-1" type="submit">Login</button>
+                
               </form>
 
         </div>
