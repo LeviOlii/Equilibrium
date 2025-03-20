3b0cb628dd3bc63ebc145788fc64d0fbad90d283
Ricardo354 <yellow_richard@proton.me>
2025-03-20 20:36:28 -0300

    adicionei cookie-parser e configurei o cors pra lidar com cookies

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const userRoutes = require('./routes/user.routes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const cors = require('cors');
 
-app.use(cors());
-
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "http://localhost:5173",
+    credentials: true
 }));
+
+app.use(cookieParser());
 
 app.use(express.json());
 app.use("/api", userRoutes);
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
