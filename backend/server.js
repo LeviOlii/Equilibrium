38d5fe934638073a8b0f81a7f5eb420b889481cd
/David <103884322+hmodavid@users.noreply.github.com>
2025-03-20 15:35:53 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,16 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
+const connectDB = require("./database");
 const testimonyRoutes = require("./routes/testimonyRoutes");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/equilibrium", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log("MongoDB conectado"))
-.catch((err) => console.error("Erro ao conectar no MongoDB", err));
+// Conectar ao banco de dados
+connectDB();
 
+// Rotas
 app.use("/api/depoimentos", testimonyRoutes);
 
 app.listen(3001, () => console.log("Servidor rodando na porta 3001"));
