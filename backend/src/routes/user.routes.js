ce620a4f09767fcb65b909e68162c7b4b4d6a06f
Ricardo354 <yellow_richard@proton.me>
2025-03-20 20:39:28 -0300

    adicionando uma rota /check-auth q retorna se o usuário está logado ou não

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -11,6 +11,14 @@ router.post("/usuarios", userController.criarUsuario);
 router.put("/usuarios/:id", userController.atualizarUsuario);
 router.delete("/usuarios:id", userController.excluirUsuario);
 
+router.get("/check-auth", auth.checkAuth, (req, res) => {
+    if (req.isLoggedIn) {
+      res.json({ isLoggedIn: true });
+    } else {
+      res.json({ isLoggedIn: false });
+    }
+  });
+
 router.post("/login", auth.login);
 
 module.exports = router;
