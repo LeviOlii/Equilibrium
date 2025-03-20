95ccb75d9d0e719c5912ac3dd9596639677a7051
/David <103884322+hmodavid@users.noreply.github.com>
2025-03-20 15:34:28 -0300

    Update testimony-model.js

--- backend/src/models/testimony-model.js
+++ backend/src/models/testimony-model.js
@@ -4,6 +4,6 @@ const TestimonySchema = new mongoose.Schema({
     name: { type: String, default: "Anônimo" },
     role: { type: String, default: "Usuário" },
     testimony: { type: String, required: true }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model("Testimony", TestimonySchema);
