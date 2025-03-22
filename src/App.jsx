f39052ed76159c5174fbb19906181563a429ce49
/David <103884322+hmodavid@users.noreply.github.com>
2025-03-22 12:53:15 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ const App = () => {
         {/*<Route path="/about" element={<About />} />
         <Route path="/groups" element={<Groups />} /> */}
         <Route path="/testimony" element={<Testimony />} />
-        <Route path="/testimony" element={<TestimonyPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SingUp />} />
         <Route path="/dashboard" element={<Dashboard />}/>
