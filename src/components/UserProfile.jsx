80753ff2e0264c7dca863fab0674eb0692296f5d
Eduarda Alves <alves.maria.es@gmail.com>
2025-03-22 23:35:30 -0300

    dados de paciente/profissional agora aparecem no perfil

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -8,7 +8,7 @@ const UserProfile = () => {
   const [error, setError] = useState('');
   const navigate = useNavigate();
 
-//colocar a opção de editar os dados tbm
+//colocar a opção de editar os dados tbm pfvvvvvv
   useEffect(() => {
     const carregarDadosUsuario = async () => {
       try {
@@ -46,14 +46,51 @@ const UserProfile = () => {
             <p className="pb-4">Essa página infoma dados sobre o usuário.</p>
             <hr />
           </div>
-          <p className="p-4">ID: {user.id}</p>
-          <hr />
-          <p className="p-4">Nome: {user.nome}</p>
-          <hr />
-          <p className="p-4">Email: {user.email}</p>
-          <hr />
-          <p className="p-4">Tipo: {user.tipo}</p>
-          <hr />
+
+          <div>
+            <hr />
+            <p className="p-4"><span className="font-bold text-desktop-bg">Nome:</span> {user.nome}</p>
+            <hr />
+            <p className="p-4"><span className="font-bold text-desktop-bg">Email:</span> {user.email}</p>
+            <hr />
+            <p className="p-4"><span className="font-bold text-desktop-bg">Tipo:</span> {user.tipo}</p>
+            <hr />
+          </div>
+
+
+          
+          {user.tipo === 'PACIENTE' && user.Paciente && (
+            <div className="mt-4">
+              <h2 className=" font-bold">Dados de Paciente</h2>
+              <p className="p-4"><span className="font-bold text-desktop-bg">Idade:</span> {user.Paciente.idade}</p>
+              <hr />
+              <p className="p-4"><span className="font-bold text-desktop-bg">Gênero:</span> {user.Paciente.genero}</p>
+              <hr />
+              <p className="p-4"><span className="font-bold text-desktop-bg">Queixas:</span> {user.Paciente.queixas}</p>
+              <hr />
+              <p className="p-4"><span className="font-bold text-desktop-bg">Histórico Familiar:</span> {user.Paciente.historico_familiar}</p>
+              <hr />
+              <p className="p-4"><span className="font-bold text-desktop-bg">Uso de Medicamentos:</span> {user.Paciente.uso_medicamentos}</p>
+              <hr />
+              <p className="p-4"><span className="font-bold text-desktop-bg">Objetivo Terapia:</span> {user.Paciente.objetivo_terapia}</p>
+            </div>
+          )}
+
+    
+          {user.tipo === 'PROFISSIONAL' && user.Profissional && (
+            <div className="mt-4">
+              <h2 className="font-bold" >Dados de Profissional</h2>
+              {user.Profissional.foto && <img src={user.Profissional.foto} alt="Foto do profissional" className="mt-4 w-32 h-32 rounded-full mx-auto" />}
+              <p className="p-4"><span className="font-bold text-desktop-bg">Especialidade:</span> {user.Profissional.especialidade}</p>
+              <hr />
+              <p className="p-4"><span className="font-bold text-desktop-bg">Localização:</span> {user.Profissional.localizacao}</p>
+              <hr />
+              <p className="p-4"><span className="font-bold text-desktop-bg">Faixa Etária de Atendimento:</span> {user.Profissional.faixa_etaria}</p>
+              <hr />
+              <p className="p-4"><span className="font-bold text-desktop-bg">Atendimentos Gratuitos:</span> {user.Profissional.atendimentos_gratuitos ? 'Sim' : 'Não'}</p>
+            </div>
+          )}
+
           <Link to="/">
                         <button className="border-2 border-desktop-bg px-6 py-2 m-2 rounded-full hover:bg-desktop-bg hover:text-brand-white transition">
                             Home
