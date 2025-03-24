55531234b56ed214bb0e915faf8bc1279f7374d6
Ricardo354 <yellow_richard@proton.me>
2025-03-23 23:46:32 -0300

    corrigindo o fato de poder editar o tipo

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -20,7 +20,7 @@ const UserProfile = ({ userId }) => {
       if (res.data.isLoggedIn) { // se ele tiver logado
         setCurrentUserId(res.data.user.id);
         if (userId && Number.isInteger(userId) && userId >= 1) { // se tiver um :id dps do /profile
-          if (res.data.user.tipo === "PACIENTE") { 
+          if (res.data.user.tipo === "PACIENTE") {
             const response = await axios.get(`http://localhost:3000/api/usuarios/${userId}`);
             if (response.data.tipo === "PACIENTE" || response.data.tipo === "ADMIN") {
               navigate('/');
@@ -70,28 +70,28 @@ const UserProfile = ({ userId }) => {
 
           <div>
             <hr />
-              <ProfileField label="Nome" value={user.nome} canEdit={currentUserId === user.id}/>
+            <ProfileField label="Nome" value={user.nome} canEdit={currentUserId === user.id} />
             <hr />
-              <ProfileField label="Email" value={user.email} canEdit={currentUserId === user.id}/>
+            <ProfileField label="Email" value={user.email} canEdit={currentUserId === user.id} />
             <hr />
-              <ProfileField label="Tipo" value={user.tipo} canEdit={currentUserId === user.id}/>
+            <ProfileField label="Tipo" value={user.tipo} canEdit={false} />
             <hr />
           </div>
 
           {user.tipo === 'PACIENTE' && user.Paciente && (
             <div className="mt-4">
               <h2 className=" font-bold">Dados de Paciente</h2>
-              <ProfileField label="Idade" value={user.Paciente.idade} canEdit={currentUserId === user.id}/>
+              <ProfileField label="Idade" value={user.Paciente.idade} canEdit={currentUserId === user.id} />
               <hr />
-              <ProfileField label="Genero" value={user.Paciente.genero} canEdit={currentUserId === user.id}/>
+              <ProfileField label="Genero" value={user.Paciente.genero} canEdit={currentUserId === user.id} />
               <hr />
-              <ProfileField label="Queixas" value={user.Paciente.queixas} canEdit={currentUserId === user.id}/>
+              <ProfileField label="Queixas" value={user.Paciente.queixas} canEdit={currentUserId === user.id} />
               <hr />
-              <ProfileField label="Queixas" value={user.Paciente.historico_familiar} canEdit={currentUserId === user.id}/>
+              <ProfileField label="Queixas" value={user.Paciente.historico_familiar} canEdit={currentUserId === user.id} />
               <hr />
-              <ProfileField label="Uso de medicamentos" value={user.Paciente.uso_medicamentos} canEdit={currentUserId === user.id}/>
+              <ProfileField label="Uso de medicamentos" value={user.Paciente.uso_medicamentos} canEdit={currentUserId === user.id} />
               <hr />
-              <ProfileField label="Objetivo da terapia" value={user.Paciente.objetivo_terapia} canEdit={currentUserId === user.id}/>
+              <ProfileField label="Objetivo da terapia" value={user.Paciente.objetivo_terapia} canEdit={currentUserId === user.id} />
             </div>
           )}
 
@@ -100,14 +100,14 @@ const UserProfile = ({ userId }) => {
             <div className="mt-4">
               <h2 className="font-bold" >Dados de Profissional</h2>
               {user.Profissional.foto && <img src={user.Profissional.foto} alt="Foto do profissional" className="mt-4 w-32 h-32 rounded-full mx-auto" />}
-              <button className="h-5 w-5 my-4"><img src="src/assets/icons/editimage.svg" alt="editar_imagem" /></button>
-              <ProfileField label="Especialidade" value={user.Profissional.especialidade} canEdit={currentUserId === user.id}/>
+              {currentUserId === user.id && (<button className="h-5 w-5 my-4"><img src="https://www.svgrepo.com/show/522527/edit-3.svg" alt="editar_imagem" /></button>)}
+              <ProfileField label="Especialidade" value={user.Profissional.especialidade} canEdit={currentUserId === user.id} />
               <hr />
-              <ProfileField label="Localização" value={user.Profissional.localizacao} canEdit={currentUserId === user.id}/>
+              <ProfileField label="Localização" value={user.Profissional.localizacao} canEdit={currentUserId === user.id} />
               <hr />
-              <ProfileField label="Faixa etária de atendimento" value={user.Profissional.faixa_etaria} canEdit={currentUserId === user.id}/>
+              <ProfileField label="Faixa etária de atendimento" value={user.Profissional.faixa_etaria} canEdit={currentUserId === user.id} />
               <hr />
-              <ProfileField label="Atendimentos gratuitos" value={user.Profissional.atendimentos_gratuitos ? 'Sim' : 'Não'} canEdit={currentUserId === user.id}/>
+              <ProfileField label="Atendimentos gratuitos" value={user.Profissional.atendimentos_gratuitos ? 'Sim' : 'Não'} canEdit={currentUserId === user.id} />
             </div>
           )}
 
