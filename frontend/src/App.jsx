e43459d8417ce2bc1d0f75126813556317090272
Ricardo354 <yellow_richard@proton.me>
2025-03-24 13:35:37 -0300

    organizando

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
 // import About from "./pages/About";
 // import Groups from "./pages/Groups";
 import Login from "./pages/Login.jsx";
